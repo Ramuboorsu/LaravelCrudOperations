54736cc90ee251698da7d080a348a262fbcc171b
unknown <ramunani0077@gmail.com>
2020-05-24 12:16:18 +0530

    version 1.0 New laravel crud operations

---
+++ app/Http/Controllers/CrudOperationsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\crudOperations;
+use Illuminate\Http\Request;
+
+class CrudOperationsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\crudOperations  $crudOperations
+     * @return \Illuminate\Http\Response
+     */
+    public function show(crudOperations $crudOperations)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\crudOperations  $crudOperations
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(crudOperations $crudOperations)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\crudOperations  $crudOperations
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, crudOperations $crudOperations)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\crudOperations  $crudOperations
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(crudOperations $crudOperations)
+    {
+        //
+    }
+}
---
+++ database/factories/CrudOperationsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\crudOperations;
+use Faker\Generator as Faker;
+
+$factory->define(crudOperations::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
